75c31cd65f50c590ba49fd5fdec199f606fcc69d
assassinki <assassinki211@gmail.com>
2025-07-14 21:53:57 -0700

    Enable SSL for PostgreSQL connection
    -update the db.js

--- src/db.ts
+++ src/db.ts
@@ -8,6 +8,9 @@ export const pool = new Pool({
   host: process.env.PGHOST,
   database: process.env.PGDATABASE,
   password: process.env.PGPASSWORD,
-  port: Number(process.env.PGPORT)
+  port: Number(process.env.PGPORT),
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
